8eed1b5bf5d5e78b94b7c1f8d2dfa1cd94eab300
Homrts <hom620@gmail.com>
2024-04-05 19:20:52 +0200

    hw

---
+++ src/components/FriendsList/FriendsList.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './FriendsStyle.module.css';
+
+export const FriendsList = ({ friends }) => {
+return (
+    <>
+    <ul className={css.friend_list}>
+        {friends.map((friend, index) => (
+        <li className={css.person} key={index}>
+            <span
+            className={css.status}
+            style={{
+            backgroundColor: `${friend.isOnline ? 'green' : 'red'}`,
+            }}
+            ></span>
+            <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+            />
+            <p className={css.name}>{friend.name}</p>
+        </li>
+        ))}
+    </ul>
+    </>
+);
+};
+
+FriendsList.propTypes = {
+friends: PropTypes.arrayOf(
+    PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    })
+).isRequired,
+};
---
+++ src/components/Transactions/Transactions.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import css from './TransactionsStyle.module.css';
+
+export const Transactions = ({ items }) => {
+return (
+    <>
+    <div className={css.container}>
+        <table className={css.transaction_history}>
+        <thead>
+            <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {items.map((item, index) => (
+            <tr key={index}>
+                <td>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+            </tr>
+            ))}
+        </tbody>
+        </table>
+    </div>
+    </>
+);
+};
+Transactions.propTypes = {
+items: PropTypes.arrayOf(
+    PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    })
+).isRequired,
+};
